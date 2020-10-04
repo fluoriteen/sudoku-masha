b264cb1ff704dfb54a7243934804dd8133c583bc
Ivanna Kolisnyk <ikolisnyk@star.global>
2020-10-05 02:38:30 +0300

    Add tests: multiple-solution puzzles

--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,6 @@
 # https://norvig.com/sudoku.html
 Tests9x9 = open('tests/9x9.txt', 'r').read().split('\n')
 Tests16x16 = open('tests/16x16.txt', 'r').read().split('\n')
+
+# same pattern of clues
+Tests9x9_2 = open('tests/9x9-2.txt', 'r').read().split('\n')
