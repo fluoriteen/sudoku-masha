9e0818e1d5081759f8c335531e83c316df1a602d
Ivanna Kolisnyk <ikolisnyk@star.global>
2020-10-05 03:03:24 +0300

    Added more same pattern puzzles

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,7 @@
 # https://norvig.com/sudoku.html
-Tests9x9 = open('tests/9x9.txt', 'r').read().split('\n')
-Tests16x16 = open('tests/16x16.txt', 'r').read().split('\n')
+path = 'tests/'
+Tests9x9 = open(path+'9x9.txt', 'r').read().split('\n')
+Tests16x16 = open(path+'16x16.txt', 'r').read().split('\n')
 
 # same pattern of clues
-Tests9x9_2 = open('tests/9x9-2.txt', 'r').read().split('\n')
+Tests9x9_2 = open(path+'9x9-2.txt', 'r').read().split('\n')
